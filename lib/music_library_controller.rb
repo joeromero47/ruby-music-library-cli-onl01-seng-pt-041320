031ccf9ad532910a3660375c4e000c84fdfd3065
Joseph Romero <joeromero47@gmail.com>
2020-05-25 19:32:57 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,9 +18,6 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip 
-      if input = 'list songs'
-        list_songs 
-      end 
     end 
   end 
   
